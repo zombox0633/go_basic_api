021abbd90da3501d5a46aff41ac88be2f2b06210
Zombox <ghostzb@outlook.com>
2025-03-25 15:53:58 +0700

    edit ErrorHandle

--- utils/handleAPI.go
+++ utils/handleAPI.go
@@ -1,17 +1,31 @@
 package utils
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
-func HandleWriteHeader(w http.ResponseWriter, err error, statusCode int) {
+func ErrorHandle(w http.ResponseWriter, err error, statusCode int) {
 	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(statusCode)
 
-		errMessage := fmt.Sprintf(`{"error": "HTTP Status %d"}`, statusCode) // สร้าง JSON message
-		w.Write([]byte(errMessage))                                          // ส่งข้อความ error
+		// แบบเก่า
+		//errMessage := fmt.Sprintf(`{"error": "HTTP Status %d"}`, statusCode) // สร้าง JSON message
+		//w.Write([]byte(errMessage))                                          // ส่งข้อความ error
 
-		return
+		errResponse := struct {
+			Error   string `json:"error"`             // เอาไว้ใส่ statusCode
+			Message string `json:"message,omitempty"` // รายละเอียดข้อผิดพลาด
+		}{
+			Error: http.StatusText(statusCode),
+		}
+
+		errResponse.Message = err.Error()
+
+		//วิธีที่สะอาดและมีประสิทธิภาพในการเขียน JSON response
+		//แปลง struct เป็น JSON และเขียนลงใน response writer โดยตรง
+		json.NewEncoder(w).Encode(errResponse)
+		// return
 	}
 }
